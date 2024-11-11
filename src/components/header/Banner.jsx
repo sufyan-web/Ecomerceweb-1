cc84d8d55ce81a4fedf197a98aa27ac853a034a8
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-11 12:46:00 +0500

    banner done

---
+++ src/components/header/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <>
+            <div className='bg-black w-full h-12 flex justify-center items-center'>
+                <p className='text-white font-font-poppins-poppinsns text-sm'>
+                    Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+                </p>
+
+            </div>
+        </>
+    )
+}
+
+export default Banner
